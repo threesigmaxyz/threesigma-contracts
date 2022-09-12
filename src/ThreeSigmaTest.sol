2b5da53dd5c0ce20027a9c6cd65c5bcc0ae5682f
3s-zemaria <zemaria@threesigma.xyz>
2022-09-12 18:22:52 +0800

    Slight error in general Test

--- src/ThreeSigmaTest.sol
+++ src/ThreeSigmaTest.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.13;
 
-import "extensions/MerkleTree"
+import "src/extensions/MerkleTree.sol";
 
 contract ThreeSigmaTest is MerkleTreeHelper {
     // General-Purpose contract for all of ThreeSigma's Foundry test helpers
